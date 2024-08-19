807e9d534d01d576261eccc21d1ce9a41d89c49f
Robin Ryan <robin1ryan@hotmail.com>
2024-08-19 20:12:41 +0200

    sorted basic layout for ui

--- src/index.js
+++ src/index.js
@@ -26,6 +26,17 @@ const CreateEl = (typeOfEl) => {
   };
 };
 
+// create factory function that adds notes which specifically has
+// some values & appends to same element.
+// has remove button, "done" button, edit button etc.
+
+// home shows top notes
+// ToDo's show all notes
+// Faq will be changed
+
+// add general information & settings to left sidebar
+// add random notes to right or something
+
 export const mainHeader = CreateEl("header");
 mainHeader.appendTo(document.body);
 
@@ -39,28 +50,91 @@ homeBtn.addText("Home");
 
 const infoBtn = CreateEl("button");
 infoBtn.appendTo(buttonDiv.el);
-infoBtn.addText("Info");
+infoBtn.addText("ToDo's");
 
 const faqBtn = CreateEl("button");
 faqBtn.appendTo(buttonDiv.el);
 faqBtn.addText("Faq");
 
+// main-body container
+const bodyContainer = CreateEl("div");
+bodyContainer.appendTo(document.body);
+bodyContainer.addId("bodyContainer");
+
+// right container
+const bodyContainerRight = CreateEl("div");
+bodyContainerRight.appendTo(bodyContainer.el);
+bodyContainerRight.addId("rightContent");
+
+const rightContentOne = CreateEl("div");
+rightContentOne.appendTo(bodyContainerRight.el);
+rightContentOne.addId("rightOne");
+
+const rightContentTwo = CreateEl("div");
+rightContentTwo.appendTo(bodyContainerRight.el);
+rightContentTwo.addId("rightTwo");
+
+const rightContentThree = CreateEl("div");
+rightContentThree.appendTo(bodyContainerRight.el);
+rightContentThree.addId("rightThree");
+
+// center container
+const bodyContainerCenter = CreateEl("div");
+bodyContainerCenter.appendTo(bodyContainer.el);
+bodyContainerCenter.addId("mainContent");
+
+// left container
+const bodyContainerLeft = CreateEl("div");
+bodyContainerLeft.appendTo(bodyContainer.el);
+bodyContainerLeft.addId("leftContent");
+
+const leftContentOne = CreateEl("div");
+leftContentOne.appendTo(bodyContainerLeft.el);
+leftContentOne.addId("leftOne");
+
+const footer = CreateEl("footer");
+footer.appendTo(document.body);
+footer.addText("Footer");
+
+// --------------- // --------------- v svg v --------------- // --------------- // --------------- //
+// might export to seperate file
+
+const iconDiv = CreateEl("div");
+iconDiv.appendTo(mainHeader.el);
+iconDiv.addId("iconDiv");
+
 export const svgDiv = CreateEl("div");
-mainHeader.el.appendChild(svgDiv.el);
+svgDiv.appendTo(iconDiv.el);
 svgDiv.addId("svgDiv");
 
 const { startFrame, endFrame } = loadAnimation();
 
 let isOpen = true;
 
-svgDiv.el.addEventListener("click", () => {
+const clickEvent = (event) => {
+  if (event.button === 0) {
+    if (!isOpen) {
+      endFrame();
+      isOpen = true;
+      buttonDiv.el.style.animation = "menuDisappear 0.4s ease forwards";
+    } else {
+      startFrame();
+      isOpen = false;
+      buttonDiv.el.style.animation = "menuBarAppear 0.4s ease forwards";
+    }
+  }
+};
+
+svgDiv.el.addEventListener("mousedown", clickEvent);
+
+buttonDiv.el.addEventListener("mouseleave", () => {
   if (!isOpen) {
-    endFrame();
-    isOpen = true;
-    buttonDiv.el.style.animation = "menuDisappear 1s ease-in forwards";
-  } else {
-    startFrame();
-    isOpen = false;
-    buttonDiv.el.style.animation = "menuBarAppear 1s ease-in forwards";
+    setTimeout(() => {
+      endFrame();
+      isOpen = true;
+      buttonDiv.el.style.animation = "menuDisappear 0.4s ease forwards";
+    }, 200);
   }
 });
+
+// --------------- // --------------- // --------------- // --------------- // --------------- //
